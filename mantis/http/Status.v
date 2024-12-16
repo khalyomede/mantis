aae6310812fc091a8cfc082e3e05f0e827c8269e
khalyomede <khalyomede@gmail.com>
2024-12-16 22:30:25 +0100

    add more HTTP status codes

--- mantis/http/Status.v
+++ mantis/http/Status.v
@@ -2,25 +2,34 @@ module http
 
 pub enum Status {
     ok
+    created
     found
+    bad_request
     not_found
+    unprocessable_entity
     server_error
 }
 
 pub fn (status Status) to_string() string {
     return match status {
-        .ok             { "OK" }
-        .found          { "FOUND" }
-        .not_found      { "NOT FOUND" }
-        .server_error   { "SERVER ERROR" }
+        .ok                     { "OK" }
+        .created                { "CREATED" }
+        .found                  { "FOUND" }
+        .bad_request            { "BAD REQUEST" }
+        .not_found              { "NOT FOUND" }
+        .unprocessable_entity   { "UNPROCESSABLE ENTITY" }
+        .server_error           { "SERVER ERROR" }
     }
 }
 
 pub fn (status Status) to_int() int {
     return match status {
-        .ok             { 200 }
-        .found          { 302 }
-        .not_found      { 404 }
-        .server_error   { 500 }
+        .ok                     { 200 }
+        .created                { 201 }
+        .found                  { 302 }
+        .bad_request            { 400 }
+        .unprocessable_entity   { 400 }
+        .not_found              { 404 }
+        .server_error           { 500 }
     }
 }
