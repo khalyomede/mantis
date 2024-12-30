65a7c6c9f0cc43e08988adb8353d70f1cb85164e
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-12-30 10:30:41 +0100

    added connect method to the method enum

--- http/Method.v
+++ http/Method.v
@@ -1,12 +1,14 @@
 module http
 
 pub enum Method {
+    connect
     get
     post
 }
 
 pub fn (method Method) to_string() string {
     return match method {
+        .connect    { "CONNECT" }
         .get        { "GET" }
         .post       { "POST" }
     }
