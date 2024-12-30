81923456fb06b3e241abbbb1b29723b7fc53fe3a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-12-30 10:33:30 +0100

    added tests for the head method

--- http/Method_test.v
+++ http/Method_test.v
@@ -4,3 +4,7 @@ fn test_method_get_to_string_return_as_expected() {
     expect(Method.get.to_string()).to_be_equal_to("GET")
 }
 
+fn test_method_head_to_string_return_as_expected() {
+    expect(Method.head.to_string()).to_be_equal_to("HEAD")
+}
+
