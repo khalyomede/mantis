c11057f14dd48cbfb4eff8a1a1b93f528e7b5492
khalyomede <khalyomede@gmail.com>
2025-01-08 20:54:16 +0100

    fix unused enum in test

--- http/HeaderType_test.v
+++ http/HeaderType_test.v
@@ -1,14 +1,14 @@
-import http { HeaderType }
+import http
 import test { expect }
 
 fn test_it_renders_header_type_accept_to_string() {
-    expect(HeaderType.accept.to_string()).to_be_equal_to("Accept")
+    expect(http.HeaderType.accept.to_string()).to_be_equal_to("Accept")
 }
 
 fn test_it_renders_header_type_content_type_to_string() {
-    expect(HeaderType.content_type.to_string()).to_be_equal_to("Content-Type")
+    expect(http.HeaderType.content_type.to_string()).to_be_equal_to("Content-Type")
 }
 
 fn test_it_renders_header_type_location_to_string() {
-    expect(HeaderType.location.to_string()).to_be_equal_to("Location")
+    expect(http.HeaderType.location.to_string()).to_be_equal_to("Location")
 }
--- http/Mime_test.v
+++ http/Mime_test.v
@@ -1,10 +1,10 @@
-import http { Mime }
+import http
 import test { expect }
 
 fn test_it_can_render_mime_application_json_to_string() {
-    expect(Mime.application_json.to_string()).to_be_equal_to("application/json")
+    expect(http.Mime.application_json.to_string()).to_be_equal_to("application/json")
 }
 
 fn test_it_can_render_mime_text_html_to_string() {
-    expect(Mime.text_html.to_string()).to_be_equal_to("text/html")
+    expect(http.Mime.text_html.to_string()).to_be_equal_to("text/html")
 }
